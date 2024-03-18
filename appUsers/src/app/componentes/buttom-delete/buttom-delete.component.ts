ce16eb0128f9c5d63be2e77ce232befa70611489
Lara Martin <lara.martin.lagares@gmail.com>
2024-03-18 17:30:36 +0100

    Creamos por línea de comandos todos los componentes que vamos a necesitar en carpeta "componentes":  butomDelete, buttomsBar, header y userCard

---
+++ appUsers/src/app/componentes/buttom-delete/buttom-delete.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-buttom-delete',
+  standalone: true,
+  imports: [],
+  templateUrl: './buttom-delete.component.html',
+  styleUrl: './buttom-delete.component.css'
+})
+export class ButtomDeleteComponent {
+
+}
---
+++ appUsers/src/app/componentes/buttoms-bar/buttoms-bar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-buttoms-bar',
+  standalone: true,
+  imports: [],
+  templateUrl: './buttoms-bar.component.html',
+  styleUrl: './buttoms-bar.component.css'
+})
+export class ButtomsBarComponent {
+
+}
