13ff2d2a5851ac72afcadeff6faf930a481836e2
Lara Martin <lara.martin.lagares@gmail.com>
2024-03-18 17:59:41 +0100

    TS - importamos en userView el componente hijo  buttomsBar .  Inyectamos ActivatedRouted para poder extraer el identificador de usuario por query params. Inyectamos el servicio users para poder utilizar los métodos creados para interactuar con la API.

--- appUsers/src/app/pages/user-view/user-view.component.ts
+++ appUsers/src/app/pages/user-view/user-view.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IUser } from '../../interfaces/iuser.interface';
+import { UsersService } from '../../services/users.service';
+import { ButtomsBarComponent } from '../../componentes/buttoms-bar/buttoms-bar.component';
 
 @Component({
   selector: 'app-user-view',
   standalone: true,
-  imports: [],
+  imports: [ButtomsBarComponent],
   templateUrl: './user-view.component.html',
   styleUrl: './user-view.component.css'
 })
+
 export class UserViewComponent {
 
+  activatedRoute = inject(ActivatedRoute);
+
+  userService = inject(UsersService);
+
+  myUser!: IUser;
+
+  router = inject(Router)
+
+  ngOnInit(): void{
+    this.activatedRoute.params.subscribe(async (params:any)=>{
+    const id = params._id  
+    try{
+      const response = await this.userService.getById(id);
+      if(response._id){
+        this.myUser = response;
+      }else{
+        this.router.navigate(['/error']);
+      }
+    }catch(err){
+      this.router.navigate(['/error']);
+    }
+    })
+  }
 }
+
