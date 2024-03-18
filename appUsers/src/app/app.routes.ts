e04e356f4d73370b7879b794693329e5718cb782
Lara Martin <lara.martin.lagares@gmail.com>
2024-03-18 17:36:53 +0100

    Damos de alta todas las rutas que vamos a usar con los componentes que cargarán en cada caso en el fichero app.routes

--- appUsers/src/app/app.routes.ts
+++ appUsers/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { UsersListComponent } from './pages/users-list/users-list.component';
+import { UserFormComponent } from './pages/user-form/user-form.component';
+import { UserViewComponent } from './pages/user-view/user-view.component';
+import { Error404Component } from './pages/error404/error404.component';
+import { ErrorAPIComponent } from './pages/error-api/error-api.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", pathMatch: "full", redirectTo:"home"},
+    { path: "home", component: UsersListComponent},
+    { path: "newuser", component: UserFormComponent},
+    { path: "user/:_id", component: UserViewComponent},
+    { path: "updateuser/:_id", component: UserFormComponent},
+    { path: "error", component: ErrorAPIComponent},
+    { path: "**", component: Error404Component} 
+
+];
